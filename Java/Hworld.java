3126eb53841bf78e84955fce339ddf346ced2170
timserverius <timserverius@gmail.com>
2012-12-02 06:59:20 -0500

    Update Java/Hworld.java

--- Java/Hworld.java
+++ Java/Hworld.java
@@ -6,5 +6,6 @@ class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
 	System.out.println("Test This");
+	System.out.println("Another line");
     }
 }
